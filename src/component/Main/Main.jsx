2ed74651c090f37f75a3ffe6969c00ae7e14b244
Jasurbek <jasurbekzayniddinov117@gmail.com>
2024-09-17 16:38:14 +0500

    fortfolio

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -1,5 +1,6 @@
   import React from "react";
   import "./Main.css";
+  import CV from "./Black Modern Professional Resume (2).pdf"
 
   const Main = () => {
     return (
@@ -8,7 +9,8 @@
           <h2>Hi, I am <br/> Zayniddinov Jasurbek</h2>
           <p>Frontend Developer</p>
           <span className="main-button">
-            <button className="Download">Download CV</button>
+            <a href={CV} download> <button className="Download">Download CV</button></a>
+           
             <button className="Learn">Learn More</button>
           </span>
         </div>
